82a0a6dc1d4cd622ebb67b1e6ccffe6b8bedf1ea
Stanley Winata <winatastanley355@gmail.com>
2022-06-11 11:52:00 +0700

    cors

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 #[macro_use]
 extern crate diesel;
 
+use actix_cors::Cors;
 use actix_web::{web, App, HttpServer};
 use dotenv::dotenv;
 use std::env;
 
 mod db;
+mod lib;
 mod migration;
 mod schema;
 mod user;
-mod lib;
 
 async fn serve_web(address: String, pool: db::PgPool) -> std::io::Result<()> {
     HttpServer::new(move || {
+        let cors = Cors::default()
+            .allow_any_header()
+            .allow_any_method()
+            .allow_any_origin();
+
         App::new()
+            .wrap(cors)
             .app_data(web::Data::new(pool.clone()))
             .route("/api/migration", web::post().to(migration::migrate))
             .service(web::scope("/api/users").configure(user::handler::route))
